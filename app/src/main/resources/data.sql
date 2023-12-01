3bd15fd39742a03ea6ab2f9f74d79129649ed31e
gabrielfernando23 <gabriel.ssilva@sptech.school>
2023-11-30 23:31:35 -0300

    att

---
+++ app/src/main/resources/data.sql
@@ -0,0 +1,12 @@
+INSERT INTO competencia(descricao)
+VALUES (('Fisioterapia'),
+        ('Psicologia'),
+        ('Terapia Ocupacional'),
+        ('Odontologia'),
+        ('Educação Física'),
+        ('Farmácia'),
+        ('Fonoaudiologia'),
+        ('Enfermagem'),
+        ('Nutrição'),
+        ('Assistência Social'),
+        ('Medicina'));
